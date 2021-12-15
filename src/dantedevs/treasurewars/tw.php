a0c1357f84e8c5897af14b0ed00f61c2560209b0
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-15 13:35:38 +0200

    Update tw.php

--- src/dantedevs/treasurewars/tw.php
+++ src/dantedevs/treasurewars/tw.php
@@ -4,7 +4,7 @@ namespace dantedevs\treasurewars;
 
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{ 
+class tw extends PluginBase{ 
 
   public function onEnable(){
     $this->getLogger()->info("§aEnabled TreasureWars §c(Work in progress!)");
